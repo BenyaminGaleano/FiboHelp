8e804296277cf30780a36e6b99bd3d640feddea5
Benyamin Galeano <benyamin.galeano@pm.me>
2019-12-30 11:44:47 -0600

    clase de chino terminada

--- Persona.java
+++ Persona.java
@@ -6,12 +6,19 @@ public class Persona {
     String apellido;
     int edad;
     Genero genero;
+    double precio;
 
-    public Persona(String primerNombre, String apellido, int edad, Genero genero){
+    public Persona(String primerNombre, String apellido, int edad, Genero genero, double precio){
         this.primerNombre = primerNombre;
         this.apellido = apellido;
         this.edad = edad;
         this.genero = genero;
+        this.precio = precio;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(precio == 0?"%s %s edad: %d genero: %s precio: %s":"%s %s edad: %d genero: %s precio: %.2f", primerNombre, apellido, edad, genero, precio==0?"Gratis":precio);
     }
 
     public enum Genero{Masculino,Femenino}
